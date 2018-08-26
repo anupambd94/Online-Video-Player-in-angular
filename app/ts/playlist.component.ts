61a31e6b045ba268deadc58eb42885b9ee5a4ba7
anupambd94 <anupam.bd94@gmail.com>
2018-08-26 12:29:56 +0600

    Added a playsist component and a html page for the component

---
+++ app/ts/playlist.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+import {config} from './config.service';
+
+@Component({
+    selector: 'paylist',
+    templateurl: 'app/ts/palylist.component.html' // Data Baindings;
+})
+
+export class PlaylistComponent {
+}
