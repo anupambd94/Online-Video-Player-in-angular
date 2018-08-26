c8d706b50bd27ca06d71edc8b3ba07249e115e85
anupambd94 <anupam.bd94@gmail.com>
2018-08-26 12:35:12 +0600

    imported te new added files to the main component

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from 'angular2/core';
 import {config} from './config.service';
-
+import {video} from './video'
+import {PlaylistComponent} from './PlaylistComponent'
 @Component({
     selector: 'my-app',
     template: '<h1>{{minHeading}}</h1>' // Data Baindings;
