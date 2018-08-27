eb6deabf0d8ee486bf84e337619de35bc77bcf7d
anupambd94 <anupam.bd94@gmail.com>
2018-08-27 22:03:41 +0600

    binding videos from the palylist and passing them

--- app/ts/playlist.component.ts
+++ app/ts/playlist.component.ts
@@ -3,7 +3,8 @@ import {config} from './config.service';
 
 @Component({
     selector: 'playlist',
-    templateUrl: 'app/ts/playlist.component.html'
+    templateUrl: 'app/ts/playlist.component.html',
+    inputs: ['videos']
 })
 
 export class PlaylistComponent {
